0ec1404b90e10b01c8740b137c76bb914b77a2f9
Leonardo Correia Damasceno <leonardo.c.damasceno@edu.senai.br>
2026-07-31 15:02:59 -0300

    feat: adiciona atividade minha estante

---
+++ 01-ambiente-node/estante.js
@@ -0,0 +1,17 @@
+// ===== LIVRO 1 =====
+const livro1 = {
+    titulo: "Clean Code",
+    autor: "Robert C. Martin",
+    preco: 89.9,
+    estoque: 12,
+};
+
+// ===== LIVRO 2 =====
+const livro2 = {
+    titulo: "Aviões: Herois do Fogo",
+    autor: "Dusty Voo Rasante",
+    preco: 99.9,
+    estoque: 0,
+};
+
+module.exports = { livro1, livro2 };
